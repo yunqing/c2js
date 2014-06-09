84363f1a3948d3da6eba7d89f77cdf54b34e2aa0
yunqing <gongyq10@gmail.com>
2014-06-09 15:17:14 +0800

    result javascript file

--- test.js
+++ test.js
@@ -4,18 +4,15 @@ function partition_3(a ,l ,h)
 	var low=l;
 	var high=h;
 	var pivotkey=a[low];
-	while(low<high)
-	{
-	while(low<high&&a[high]>=pivotkey)
-	{
-	high=high-1;
+	while(low<high) {
+		while(low<high&&a[high]>=pivotkey) {
+		high=high-1;
 	}
 	temp=a[low];
 	a[low]=a[high];
 	a[high]=temp;
-	while(low<high&&a[low]<=pivotkey)
-	{
-	low++;
+	while(low<high&&a[low]<=pivotkey) {
+		low++;
 	}
 	temp=a[low];
 	a[low]=a[high];
@@ -27,24 +24,19 @@ function partition_3(a ,l ,h)
 function qsort_3(a ,low ,high)
 {
 	var pivotloc;
-	if(low<high)
-	{
-		pivotloc=this.partition_3(a,low,high);
-	this.qsort_3(a,low,pivotloc-1);
-	this.qsort_3(a,pivotloc+1,high);
+	if(low<high) {
+		pivotloc=partition_3(a,low,high);
+	qsort_3(a,low,pivotloc-1);
+	qsort_3(a,pivotloc+1,high);
 	}
 }
 
  function main_0()
 {
-	var a=new Array(1,4,7,2,8,10,3,6,5,9);
+	var a=[1,4,7,2,8,10,3,6,5,9];
 	var n=10;
-	this.qsort_3(a,0,9);
+	qsort_3(a,0,9);
 	return 0;
 }
 
  
-Cannot find Function:
-partition_3
-Cannot find Function:
-qsort_3
